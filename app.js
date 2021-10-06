40c1b608bf2efde585649b97358caaea5661e65c
gastonsk <amayagaston.sk@gmail.com>
2021-10-06 02:00:28 -0300

    commands
    added responsive commands

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ const client = new tim.Client({
   },
 // lack of identity tags makes the bot anonymous and able to fetch messages from the channel
 // for reading, supervision, spying, or viewing purposes only
+identity: {
+  username: `${process.env.TWITCH_USERNAME}`,
+  password: `oauth:${process.env.TWITCH_OAUTH}`
+},
   channels: [`${process.env.TWITCH_CHANNEL}`]
 
 });
@@ -30,9 +34,38 @@ client.connect().catch(console.error);
 client.on('message', (channel, tags, message, self) => {
     // Lack of this statement or it's inverse (!self) will make it in active
     if (self) return;
+
+   switch (message.toLowerCase()) {
+   
+
     
-// This logs out all the messages sent on the channel on the terminal
-    console.log(message);
+      case 'commands':
+            client.say(channel, `@${tags.username}, available commands are:
+            Commands Help Greetings Hi !Website !Name
+           
+            For more help just type "Help"
+            `);
+            break;
+
+                 // In case the message in lowercase is equal to the string '!website', send the sender of that message your personal website
+        case '!website':
+          client.say(channel, `@${tags.username}, my website is www.google.com!`);
+          break;
+          
+          // In case the message in lowercase is equal to the string 'greetings', send the sender of that message 'Hello @Username, what's up?!'
+      case 'greetings':
+          client.say(channel, `Hello @${tags.username}, what's up?!`);
+          break;
+          
+          // In case the message in lowercase is equal to the string 'hi', send the sender of that message 'Username, hola'
+      case 'hi':
+          client.say(channel, `${tags.username}, hola!`);
+          break;
+   
+   
+   } 
+    
+
 
 });
 
